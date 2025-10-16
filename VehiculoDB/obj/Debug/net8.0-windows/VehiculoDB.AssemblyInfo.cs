d28b433447b00d5f174bff30264fd44727880e67
Eduardo Vásquez <eduardo.vqz968@gmail.com>
2025-10-15 20:28:56 -0600

    Clase TiposMantenimiento

--- VehiculoDB/obj/Debug/net8.0-windows/VehiculoDB.AssemblyInfo.cs
+++ VehiculoDB/obj/Debug/net8.0-windows/VehiculoDB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehiculoDB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+409bdd83fbd9aff49af347a680dbab2b89c429b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4a50ac33b897c3b82ceb17d2f22af8daf01109b")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehiculoDB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehiculoDB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

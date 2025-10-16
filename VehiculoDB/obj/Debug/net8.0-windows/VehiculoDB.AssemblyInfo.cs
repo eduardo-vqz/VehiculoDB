802946cbf6be60fd35a0e120185e2152e18a73ca
Eduardo Vásquez <eduardo.vqz968@gmail.com>
2025-10-15 21:59:48 -0600

    Form

--- VehiculoDB/obj/Debug/net8.0-windows/VehiculoDB.AssemblyInfo.cs
+++ VehiculoDB/obj/Debug/net8.0-windows/VehiculoDB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehiculoDB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d28b433447b00d5f174bff30264fd44727880e67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32a90f8dec960cc45a55e2d494b4248307dace4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehiculoDB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehiculoDB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

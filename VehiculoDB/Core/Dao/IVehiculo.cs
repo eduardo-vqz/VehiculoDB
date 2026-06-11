e5f66012048cfc999cee0e3c117ddc9ae056b5ab
Eduardo Vásquez <eduardo.vqz968@gmail.com>
2026-06-11 16:10:04 -0600

    refactor: permitir vehiculo nulo en busqueda por id

--- VehiculoDB/Core/Dao/IVehiculo.cs
+++ VehiculoDB/Core/Dao/IVehiculo.cs
@@ -1,8 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VehiculoDB.Core.Clases;
 
 namespace VehiculoDB.Core.Dao
@@ -12,7 +7,7 @@ namespace VehiculoDB.Core.Dao
         int Insert(Vehiculos paVehiculo);
         bool Update(Vehiculos paVehiculo);
         bool Delete(int idVehiculo);
-        Vehiculos GetById(int idVehiculo);
+        Vehiculos? GetById(int idVehiculo);
         List<Vehiculos> GetAll(string filtro = "");
     }
 }
